4aec29bfecd92d8282d57f631f9877b6d9c95977
lebbysam200 <samsonbatendi@hotmail.com>
2015-06-25 14:12:09 +0200

    base identifier tests

--- edc_identifier/tests/test_base_identifier_methods.py
+++ edc_identifier/tests/test_base_identifier_methods.py
@@ -25,27 +25,32 @@ class TestModelTestCase(TestCase):
         self.assertEqual(self.identifier.get_check_digit(base_new_identifier = '038-12990002'),'038-12990002')
         
     def test_get_check_digit1(self):
+        """asserts that get_check_digit adds a base check digit as required"""
         self.identifier.add_check_digit = True
         self.assertEqual(self.identifier.get_check_digit(base_new_identifier = '038-12990002'),'038-12990002-3')
     
     def test_get_identifier(self):
+        """asserts that the first identifier created is 066-12990001-2"""
         add_check_digit = True
-        """assert that the first identifier created is 066-12990001-2"""
         self.assertEqual(self.identifier.get_identifier(add_check_digit=add_check_digit),'066-12990001-2')
     
     def test_get_identifier1(self):
+        """asserts that no check digit is added if not needed"""
         add_check_digit = False
         self.assertEqual(self.identifier.get_identifier(add_check_digit=add_check_digit),'066-12990001')
         
     def test_get_identifier2(self):
+        """asserts that setting add_check_digit to none and trying to get an identifier raises an attribute error"""
         self.identifier.add_check_digit = None
         self.assertRaises(AttributeError,self.identifier.get_identifier,self.identifier.add_check_digit)
         
     def test_get_identifier3(self):
+        """asserts that trying to get an identifier with is_derived as none raises an attribute error"""
         self.identifier.is_derived = None
         self.assertRaises(AttributeError,self.identifier.get_identifier,self.identifier.add_check_digit)
         
     def test_get_identifier4(self):
+        """asserts that the identifiers created are unique"""
         add_check_digit = True
         identifier1 = self.identifier.get_identifier(add_check_digit=add_check_digit)
         self.assertNotEqual(self.identifier.get_identifier(add_check_digit=add_check_digit),identifier1)
