79bb4a2780532ddc33992a3bfcfdae9105cee629
lebbysam200 <samsonbatendi@hotmail.com>
2015-06-26 09:10:22 +0200

    infant_identifier tests

---
+++ edc_identifier/tests/test_infant_identifier_methods.py
@@ -0,0 +1,63 @@
+from django.test import TestCase
+from django.db import models
+from ..classes import InfantIdentifier, SubjectIdentifier
+
+from ..models import BaseIdentifierModel
+
+from ..exceptions import IdentifierError
+
+class StudySite(object):
+    def __init__(self,site_code=None,site_name=None):
+        self.site_code = site_code
+        self.site_name = site_name 
+    
+    class Meta:
+        app_label = 'edc_identifier'
+
+class InfantIdentifierModel(BaseIdentifierModel):
+
+    objects = models.Manager()
+
+    class Meta:
+        app_label = 'edc_identifier'
+        ordering = ['-created']
+
+class TestModelTestCase(TestCase):
+    
+    def setUp(self):
+        
+        study_site = StudySite(site_code='19', site_name='Molepolole')
+        
+        is_derived = False
+        add_check_digit = False
+        self.identifier = SubjectIdentifier(model_name = 'subjectidentifier', app_name = 'edc_identifier', identifier_prefix = '066',
+                                            is_derived=is_derived, site_code='12', add_check_digit=add_check_digit)
+        self.identifier.modulus = 7
+        maternalId = self.identifier.get_identifier(add_check_digit=True)
+        self.infantId = InfantIdentifier(model_name = 'infantidentifiermodel', app_name = 'edc_identifier',maternal_identifier=maternalId, 
+                                         study_site=study_site, birth_order=2, live_infants=3,live_infants_to_register=3,modulus=7)
+    
+    def test_get_identifier_prep(self):
+        idPrep = {'suffix': 56, 'maternal_identifier': '066-12990001-2'}
+        self.assertEqual(self.infantId.get_identifier_prep(),idPrep)
+    
+    def test_get_identifier_prep0(self):
+        self.infantId.maternal_identifier='056-19800001-3'
+        self.assertRaises(IdentifierError,self.infantId.get_identifier_prep)
+    
+    def test_get_identifier_prep1(self):
+        self.infantId.live_infants_to_register=4
+        self.assertRaises(IdentifierError,self.infantId.get_identifier_prep)
+        
+    def test_get_identifier_prep2(self):
+        self.infantId.live_infants=0
+        self.assertRaises(IdentifierError,self.infantId.get_identifier_prep)
+        
+    def test_get_suffix(self):
+        self.assertEqual(self.infantId._get_suffix(),56)
+        
+    def test_base_suffix(self):
+        self.assertEqual(self.infantId._get_base_suffix(),36)
+        
+    def test_get_identifier_post(self):
+        print(self.infantId.get_identifier_post(new_identifier='066-12990001-2-56'))
--- edc_identifier/tests/test_subject_identifier.py
+++ edc_identifier/tests/test_subject_identifier.py
@@ -1,15 +1,15 @@
 from uuid import UUID
-
+ 
 from django.db.utils import IntegrityError
 from django.test import TestCase
-
+ 
 from ..exceptions import IdentifierError
-
+ 
 from edc_registration.models import RegisteredSubject
-
-
+ 
+ 
 class TestSubjectIdentifier(TestCase):
-
+ 
     def test_subject_identifier_not_none(self):
         """Asserts subject identifier cannot be None after save()."""
         rs = RegisteredSubject()
@@ -17,21 +17,21 @@ class TestSubjectIdentifier(TestCase):
         self.assertTrue(not rs.subject_identifier)
         rs.save()
         self.assertEquals(rs.subject_identifier, str(rs.subject_identifier_as_pk))
-
+ 
     def test_subject_identifier_is_pk(self):
         """Asserts subject identifier is a uuid after save()."""
         rs = RegisteredSubject()
         self.assertTrue(isinstance(rs.subject_identifier_as_pk, UUID))
         rs.save()
         self.assertEquals(rs.subject_identifier, str(rs.subject_identifier_as_pk))
-
+ 
     def test_subject_identifier_as_pk_not_none(self):
         """Asserts subject identifier is a uuid after save()."""
         rs = RegisteredSubject()
         self.assertTrue(isinstance(rs.subject_identifier_as_pk, UUID))
         rs.subject_identifier_as_pk = None
         self.assertRaises(ValueError, rs.save)
-
+ 
     def test_subject_identifier_constraint(self):
         """Asserts cannot accept duplicate on as_pk."""
         rs = RegisteredSubject()
@@ -39,7 +39,7 @@ class TestSubjectIdentifier(TestCase):
         dup = rs.subject_identifier_as_pk
         rs = RegisteredSubject(subject_identifier_as_pk=dup)
         self.assertRaises(IntegrityError, rs.save)
-
+ 
     def test_duplicate_subject_identifier(self):
         subject_identifier = '123456789'
         rs = RegisteredSubject(subject_identifier=subject_identifier)
